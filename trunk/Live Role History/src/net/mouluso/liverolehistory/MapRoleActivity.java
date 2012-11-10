4951da296ee296dedac2470b9c3c706fa18dd99a
dhmourelle@gmail.com <dhmourelle@gmail.com@eff60205-f513-b641-24b4-656ebcbe3d26>
2012-11-10 23:52:38 +0000

    Broza and more broza

--- trunk/Live Role History/src/net/mouluso/liverolehistory/MapRoleActivity.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/MapRoleActivity.java
@@ -13,6 +13,11 @@ import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
@@ -23,14 +28,28 @@ import com.google.android.maps.OverlayItem;
 
 public class MapRoleActivity extends MapActivity implements GPSReadable{
 
+	private static final int MESSAGE_DIALOG = 0;
+	private static final int ENTER_DATA_DIALOG = 1;
+	
 	private MapView mv;
 	private Location location;
 	private GPSListener gpsListener;
 	private GeoPoint locationToLocate;
 	private MyLocationOverlay markOverlay;
 	private OverlayItem mark;
+	private RelativeLayout shadow;
+	private Button locateMeButton;
+	private Button checkLocationButton;
+	private Button showHintButton;
+	private ImageView dialogIcon;
+	private TextView dialogMessage;
+	private EditText dialogEnterData;
+	private Button actionMessageButton;
+	private Button searchInfoMessageButton;
 	
-    @Override
+    
+	private int messageType;
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
@@ -38,14 +57,28 @@ public class MapRoleActivity extends MapActivity implements GPSReadable{
         mv.setSatellite(true);
         mv.getController().setZoom(20);
         this.gpsListener = new GPSListener(getApplicationContext(), this);
-        DatabaseOperations.getInstance(getApplicationContext()).insertLocation();
-        locationToLocate = DatabaseOperations.getInstance(getApplicationContext()).getLocation();
+
+        locationToLocate = new GeoPoint(45367824, 8908765);
+        
         
         List<Overlay> mapOverlays = mv.getOverlays();
         Drawable markDrawable = this.getResources().getDrawable(R.drawable.me_location);
         markOverlay = new MyLocationOverlay(markDrawable, this);
         mapOverlays.add(markOverlay);
         
+        shadow = (RelativeLayout) findViewById(R.id.shadow_layout);
+        shadow.setVisibility(RelativeLayout.INVISIBLE);
+        
+        locateMeButton = (Button) findViewById(R.id.locate_me);
+        checkLocationButton = (Button) findViewById(R.id.check_location);
+        showHintButton = (Button) findViewById(R.id.show_hint);
+        
+        dialogIcon = (ImageView) findViewById(R.id.icon_dialog);
+    	dialogMessage = (TextView)findViewById(R.id.message_dialog);
+    	dialogEnterData = (EditText) findViewById(R.id.message_enter_data);
+    	
+    	actionMessageButton = (Button) findViewById(R.id.action_dialog);
+    	searchInfoMessageButton = (Button) findViewById(R.id.search_dialog);
     }
 
     @Override
@@ -95,6 +128,57 @@ public class MapRoleActivity extends MapActivity implements GPSReadable{
 			Toast.makeText(getApplicationContext(), "Te faltan: " + distance + " metros", Toast.LENGTH_SHORT).show();
 		}
 	}
+	
+	public void showHint(View v){
+		//configMessageDialog();
+		configEnterDataDialog();
+		enableMapButtons(false);
+		shadow.setVisibility(RelativeLayout.VISIBLE);
+	}
+	
+	public void doActionDialog(View v){
+		switch(messageType){
+		case MESSAGE_DIALOG: enableMapButtons(true); shadow.setVisibility(RelativeLayout.INVISIBLE); break;
+		case ENTER_DATA_DIALOG: break;
+		}
+			
+	}
+	
+	public void searchInfo(View v){
+		
+	}
+	
+	private void enableMapButtons(boolean enabled){
+		locateMeButton.setEnabled(enabled);
+		checkLocationButton.setEnabled(enabled);
+		showHintButton.setEnabled(enabled);
+	}
+	
+	private void configMessageDialog(){
+		dialogIcon.setVisibility(RelativeLayout.VISIBLE);
+		dialogMessage.setVisibility(RelativeLayout.VISIBLE);
+		actionMessageButton.setVisibility(RelativeLayout.VISIBLE);
+		searchInfoMessageButton.setVisibility(RelativeLayout.VISIBLE);
+		
+		dialogEnterData.setVisibility(RelativeLayout.INVISIBLE);
+		messageType = MESSAGE_DIALOG;
+		
+		actionMessageButton.setText(R.string.close_action);
+		searchInfoMessageButton.setText(R.string.search_info);
+	}
+	
+	private void configEnterDataDialog(){
+		dialogIcon.setVisibility(RelativeLayout.VISIBLE);
+		dialogMessage.setVisibility(RelativeLayout.VISIBLE);
+		actionMessageButton.setVisibility(RelativeLayout.VISIBLE);
+		dialogEnterData.setVisibility(RelativeLayout.VISIBLE);
+		
+		searchInfoMessageButton.setVisibility(RelativeLayout.INVISIBLE);
+		messageType = ENTER_DATA_DIALOG;
+		
+		actionMessageButton.setText(R.string.enter_data_action);
+	}
+	
 	
 	public void setLocation(Location l) {
 		this.location = l;
