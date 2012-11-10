2a57189d8f1f6ffaef36d1cf13ec196e2ad924d7
0.luso.tm@gmail.com <0.luso.tm@gmail.com@eff60205-f513-b641-24b4-656ebcbe3d26>
2012-11-10 14:21:44 +0000

    Added SQL Table create sentences.

--- trunk/Live Role History/src/net/mouluso/liverolehistory/storage/constants/StorageConstants.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/storage/constants/StorageConstants.java
@@ -33,4 +33,59 @@ public class StorageConstants {
 //			");";
 	
 	
+	public static final String HISTORY_TABLE = "Historys";
+	public static final String EVENTS_TABLE = "Events";
+	public static final String TREASURE_TABLE = "Treasure";
+	public static final String TIMED_ACTIONS_TABLE = "TimedActions";
+	public static final String QUESTION_ACTIONS_TABLE = "QuestionActions";
+	public static final String ID = "_id";
+	public static final String NAME = "Name";
+	public static final String DESCRIPTION = "Description";
+	public static final String HISTORY_ID = "HistoryId";
+	public static final String NEXT_EVENT_ID = "NextEventId";
+	public static final String ACTION_NAME = "ActionName";
+	public static final String ACTION_ID = "ActionId";
+	public static final String TREASURE_ID = "TreasureId";
+	public static final String TYPE = "Type";
+	public static final String IMAGE = "Image";
+	public static final String TIME = "_time";
+	public static final String QUESTION = "_question";
+	public static final String ANSWER = "_answer";
+
+	public static final String CREATE_HISTORY_TABLE = 
+			"CREATE TABLE " + HISTORY_TABLE + "(" + 
+					ID + " INTEGER PRIMARY KEY," +
+					DESCRIPTION + " TEXT NOT NULL);"
+					;
+
+	public static final String CREATE_EVENTS_TABLE =
+			"CREATE TABLE " + EVENTS_TABLE + "(" +
+					ID + " INTEGER PRIMARY KEY," +
+					DESCRIPTION + " TEXT NOT NULL," +
+					HISTORY_ID + " INTEGER NOT NULL," +
+					NEXT_EVENT_ID + " INTEGER NOT NULL," +
+					ACTION_NAME + " INTEGER NOT NULL," +
+					ACTION_ID + " INTEGER NOT NULL," +
+					TREASURE_ID + " INTEGER NOT NULL," +
+					LATITUDE + " INTEGER NOT NULL," +
+					LONGITUDE + " INTEGER NOT NULL);"					
+					;
+	public static final String CREATE_TREASURE_TABLE =
+			"CREATE TABLE " + TREASURE_TABLE + "(" +
+					ID + " INTEGER PRIMARY KEY," +
+					TYPE + " TEXT NOT NULL," +
+					IMAGE + " TEXT NOT NULL);"
+					;
+	
+	public static final String CREATE_TIMEDACTIONS_TABLE =
+			"CREATE TABLE " + TIMED_ACTIONS_TABLE + "(" +
+					ID + " INTEGER PRIMARY KEY," +
+					TIME + " INTEGER NOT NULL);"
+					;
+	public static final String CREATE_QUESTIONACTIONS_TABLE = 
+			"CREATE TABLE " + QUESTION_ACTIONS_TABLE + "(" +
+					ID + " INTEGER PRIMARY KEY" +
+					QUESTION + " TEXT NOT NULL," +
+					ANSWER + " TEXT NOT NULL);"
+					;
 }
