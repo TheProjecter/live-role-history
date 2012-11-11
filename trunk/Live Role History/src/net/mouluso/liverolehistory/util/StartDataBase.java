ee9f4adcd2b37ac4a6da1920b6dd027cc8c8a24b
0.luso.tm@gmail.com <0.luso.tm@gmail.com@eff60205-f513-b641-24b4-656ebcbe3d26>
2012-11-11 05:19:52 +0000

    Added ALL THE SPOTS!

--- trunk/Live Role History/src/net/mouluso/liverolehistory/util/StartDataBase.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/util/StartDataBase.java
@@ -51,42 +51,120 @@ public class StartDataBase {
     				"Bar La Bombilla, A coruña",
     				"Eso sí, sigue pegando o sol que da gusto. Que ben me viña un xeado ahora mismo. Algo típico, caseiro, e que me quede preto, non penso andar un kilómetro para comerme un Cornetto.",
     				43370671,-8399557);
-    /*		DatabaseOperations.getInstance(context).insertEvent(
-    				description, 
-    				historyId,
-    				order,
-    				question,
-    				answer,
-    				search,
-    				success,
-    				latitude,
-    				longitude);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				description, 
-    				historyId,
-    				order,
-    				question,
-    				answer,
-    				search,
-    				success,
-    				latitude,
-    				longitude);
+    				" Que máis pode pedir un home? A barriga chea, moita cervexa, un bon xeado, unha mullero que o queir...",
+    				1,
+    				5,
+    				"En que ano se abriu a Heladería Colón?",
+    				"1973",
+    				"Heladería Colón, A Coruña",
+    				"O ANIVERSARIO .E onte nin cheguéi ó barco. Meu deus, desta mátame. Sólo me queda fuxir. Necesito un capitán cun barco que me poda levar lonxe de aquí.",
+    				43369262,-8400703);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				description, 
-    				historyId,
-    				order,
-    				question,
-    				answer,
-    				search,
-    				success,
-    				latitude,
-    				longitude);*/
-        
+    				"Bueno, se aquí naide me pode levar a augas internacionáis, ninguén pode.", 
+    				1,
+    				6,
+    				"En que ano se fundou o Real Club Náutico Da Coruña?",
+    				"1926",
+    				"Real Club Náutico de La Coruña",
+    				"Pois parece ser que onte escoitaron os berros da miña muller dende aquí e ahgora ninguén se atreve a levarme. Dinme que vaia a un centor comercial (din que hai varios para escoller, pero que non me complique) e que lle atope O REGALO PERFECTO para ela. Isto vai ser difícil.",
+    				43368454,-8397514);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Atopeino! Se isto non consegue salvarme o pescozo, nada o fará. en fin, que sexa o que Deus queira. Tomarei outra cervexa, por se acaso.", 
+    				1,
+    				7,
+    				"Cantos centros comerciáis hai na Coruña?",
+    				"6",
+    				"Centro de Ocio, El Puerto, A Coruña",
+    				"",
+    				43368113,-840039);
+    		
+    		
+    		
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				" Meses de penurias e peregrinaxe ata chegar a ver A Catedral do Santo Apóstol e unhas palabras mal interpretadas por unha bruxa métenme neste problema. Que é este sitio de enormes construccións, metal e bruxería? Que Deus se apiade da miña alma. A bruxa non me mandou demasiado lonxe, pero sí moi tarde. O última voz que escoitei mandoume buscar un reloxo alto, entre árboles e edificios.", 
+    				2,
+    				1,
+    				"En que ano se construíu o Obelisco?",
+    				"1895",
+    				"Obelisco Cantón Pequeno A Coruña",
+    				"Atopo un trozo de antracita que destaca sobor das demáis pedras. Atráeme estrañamente. Debaixo dela hai unha nota que di: Non todo está perdido. Sigue ás árbores, busca ó bardo de metal.",
+    				43368659,-8402209);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Así que este é o famoso bardo. Gustaríame escoitar algunha das súas composicións. ", 
+    				2,
+    				2,
+    				"En que ano foi asasinado John Lennon?",
+    				"1980",
+    				"Monumendo a john Lennon, A Coruña",
+    				"Na súa man, hai outra nota: Nesta cidade viveu o Pintor dos períodos coloridos e das innumerábeis obras. Vai a onde él viveu.",
+    				43367712,-8403136);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Fun preguntando polo camiño, e foi un home realmente abraiante este Picasso, gustaríame dispoñer do tempo necesario para aprender máis, pero gustaríame máis volver á casa.", 
+    				2,
+    				3,
+    				"En cantos días pintou Picasso o Guernica?",
+    				"33",
+    				"Casa Museo Picasso, A Coruña",
+    				"No seu buzón, unha nota: “Alí onde a terra divide ó mar e se alzan as bandeiras deberás continuar a túa viaxe.",
+    				 43367156,-8407197);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Este sitio é especialmente belo. Creo que ficarei aquí un anaco a meditar sobre esta estraña situación e disfrutar da paisaxe.", 
+    				2,
+    				4,
+    				"Cal é a festa máis coñecida celebrada nas praias de Orzán e Riazor?",
+    				"San xoán",
+    				"Plaias de Riazor e Orzán,  A coruña.",
+    				"A última nota que atoipei, neste mesmo banco: Busca o lugar onde tódalas artes se xuntan, non moi lonxe da relixión e o mar.",
+    				43369392,-8407937);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Resultoume especialmente complicado atopar con este sitio. Aínda así, marabilloume, a arte cambiou moito dende os meus días. ", 
+    				2,
+    				5,
+    				"En que ano se inaugurou o Museo Das Belas Artes Da Coruña?",
+    				"1995",
+    				"Museo Das Belas Artes Da Coruña",
+    				"Vai alí onde mora o Home.",
+    				43372806,-8399872);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Aquí non vive un home. Aquí vive O home.E fascíname este lugar. Tantas cousas que aprender...", 
+    				2,
+    				6,
+    				"Cal é o lema escrito ás portas do museo?",
+    				"Coñecete a ti mesmo",
+    				"Museo domus, A Coruña",
+    				"Continúa por este camiño. Tes outra morada que ir visitar.",
+    				43377511,-8406677);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Isto é realmente impresionante. E belo. Deberíase de coidar máis da natureza.", 
+    				2,
+    				7,
+    				"Como se chama a sala máis famosa do Acuarium?",
+    				"Nautilus",
+    				"Acuarium Finisterrae, A Coruña",
+    				"É posible que te atopes algo perdido. Xunto á cabeza de Xerión atoparás o Norte",
+    				43383507,-8409777);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"on todas as marabillas e cousas abraiantes que vin, resulta impresionante que unha Rosa dos Ventos non caera no olvido.", 
+    				2,
+    				8,
+    				"Como se chamaba o petroleiro que naufragou non moi lonxe de aquí en 1992?",
+    				"Mar Egeo",
+    				"Rosa dos Ventos da Torre de Hércules, A Coruña",
+    				"Creo que xa sabes o seguinte paso. Achégate ó ceo todo o que podas, e descansa",
+    				43386698,-8407727);
+    		DatabaseOperations.getInstance(context).insertEvent(
+    				"Xuntei todas as notas e parece que todo cobra sentido... Creo que a pesar de todo, voltarei á casa.", 
+    				2,
+    				9,
+    				"Cantos metros mide a Torre de Hércules?",
+    				"68",
+    				"Torre de Hércules",
+    				"",
+    				4338569,-8406633);
         
         }
-	}
-
-}
+}}
 
 //(String description, int historyId,  int order, String question, String answer, int search, String success,
 //int latitude, int longitude)
