61557dbbee657f6d46af0e3ecb154d11b1ce020f
0.luso.tm@gmail.com <0.luso.tm@gmail.com@eff60205-f513-b641-24b4-656ebcbe3d26>
2012-11-11 01:07:01 +0000

    Listview created, problems with DB

--- trunk/Live Role History/src/net/mouluso/liverolehistory/ListViewHistories.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/ListViewHistories.java
@@ -1,8 +1,13 @@
 package net.mouluso.liverolehistory;
 
+import java.util.ArrayList;
+
+import net.mouluso.liverolehistory.adapters.HistoryArrayAdapter;
+import net.mouluso.liverolehistory.model.History;
+import net.mouluso.liverolehistory.storage.DatabaseOperations;
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.ListView;
 
 public class ListViewHistories extends ListActivity {
 
@@ -10,6 +15,11 @@ public class ListViewHistories extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview_histories);
+        
+        ArrayList<History> histories = DatabaseOperations.getInstance(getApplicationContext()).getHistoryList();
+        HistoryArrayAdapter adaptador = new HistoryArrayAdapter(getApplicationContext(), histories);
+        ListView listviewHistorias = (ListView) findViewById(android.R.id.list);
+        listviewHistorias.setAdapter(adaptador);
     }
 
 }
--- trunk/Live Role History/src/net/mouluso/liverolehistory/adapters/HistoryArrayAdapter.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/adapters/HistoryArrayAdapter.java
@@ -1,6 +1,9 @@
 package net.mouluso.liverolehistory.adapters;
 
+import java.util.ArrayList;
+
 import net.mouluso.liverolehistory.R;
+import net.mouluso.liverolehistory.model.History;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class HistoryArrayAdapter extends ArrayAdapter<String> {
+public class HistoryArrayAdapter extends ArrayAdapter<History> {
 	  private final Context context;
-	  private final String[] values;
+	  private final ArrayList<History> values;
 
-	public HistoryArrayAdapter(Context context, String[] values) {
+	public HistoryArrayAdapter(Context context, ArrayList<History> values) {
 	    super(context, R.layout.listview_histories, values);
 	    this.context = context;
 	    this.values = values;
@@ -26,7 +29,10 @@ public class HistoryArrayAdapter extends ArrayAdapter<String> {
 	    View rowView = inflater.inflate(R.layout.historyadapter, parent, false);
 	    TextView textView = (TextView) rowView.findViewById(R.id.historyadapter_label);
 	    ImageView imageView = (ImageView) rowView.findViewById(R.id.historyadapter_icon);
-	    textView.setText(values[position]);
+	    textView.setText(values.get(position).getName());
+	    
+	    imageView.setImageResource(context.getResources().getIdentifier(values.get(position).getImage(),
+	    		"drawable", context.getPackageName()));
 	    
 	    return rowView;
 	  }
