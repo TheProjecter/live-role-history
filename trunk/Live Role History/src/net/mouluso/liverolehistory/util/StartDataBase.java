cb3ea769f73b7e98b984abf23890fd7224406147
0.luso.tm@gmail.com <0.luso.tm@gmail.com@eff60205-f513-b641-24b4-656ebcbe3d26>
2012-11-11 06:39:52 +0000

    Fixed a little problem with the spots

--- trunk/Live Role History/src/net/mouluso/liverolehistory/util/StartDataBase.java
+++ trunk/Live Role History/src/net/mouluso/liverolehistory/util/StartDataBase.java
@@ -21,63 +21,72 @@ public class StartDataBase {
     				1,
     				"En que ano se construíu o Obelisco?",
     				"1895",
-    				"Obelisco Cantón Pequeno A Coruña",
-    				"A ver, sei que comecei a noite cun tal Seamos en ¿Dublín? Iso non ten sentido. ¿Unha panaderia? Buf, a ver si andando me despexzo un pouco.",
+    				"Coruña monumento reloj linares",
+    				"Moi ben! O obelisco, construído en 1895 segue a ser un dos puntos de encontro máis míticos de Coruña.",
     				43368659, -8402209);
+    		    		
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Ah, carallo. Pois sí! No mesmo dublin! Xa que estou, boto unha cervexa. O camarero acaba de devolverme os lentes de sol que me olvidara onte!. Tomo outra, e xa de paso, invítoo.", 
+    				"A ver, sei que comecei a noite cun tal Seamos en ¿Dublín? Iso non ten sentido. ¿Unha panaderia? Buf, a ver si andando me despexzo un pouco.",
     				1,
     				2,
     				"De qué país é capital Dublín?",
     				"Irlanda",
-    				"Pub Dublin, A Coruña",
-    				"Moito mellor. Nin punto de comparación. Eso sí, solazo que fai. Como non encontre o protector pronto, vou pelar enteiriño. O camarero dime que fomos a visitar ó primo de Seamus, Matthews. Igual ata o ten el. Onde cona vivirá?",
+    				"Dublín Panaderas Coruña",
+    				"Ah, carallo. Pois sí! No mesmo Dublin! Un dos pubs Xa que estou, boto unha cervexa. O camarero acaba de devolverme os lentes de sol que me olvidara onte!. Tomo outra, e xa de paso, invítoo.",
     				43372417,-8399489);
+    				 
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Estaba visto. Matthews é o dono do Matthews, non era tan complicado. E ten o meu protector solar! Teño que invitalo a algo. E xa que estou, refréscome un pouquiño.", 
+    				"Moito mellor. Nin punto de comparación. Eso sí, solazo que fai. Como non encontre o protector pronto, vou pelar enteiriño. O camarero dime que fomos a visitar ó primo de Seamus, Matthews. Igual ata o ten el. Onde cona vivirá?",
     				1,
     				3,
     				"Como se chama a cervexa negra máis famosa do mundo, que podes atopar aquí?",
     				"Guinness",
-    				"Cervecería Matthews, A Coruña",
-    				"Que boa a...¿cuarta? cervexa. Cecáis debería comer algo, que onte empecéi así... Haberá por aquí algún sitio típico con tapas baratas de tortilla, albóndegas, calamares... Oh, oh, e chourizo con patacas, por suposto. E cervexa, a duda ofende",
+    				"A Coruña Matthews",
+    				"Estaba visto. Matthews é o dono do Matthews, non era tan complicado. E ten o meu protector solar! Teño que invitalo a algo. E xa que estou, refréscome un pouquiño.", 
     				43370579,-8400773);
+    		
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"ISTO XA É OUTRO CONTO. Onde vai parar. Creo que xa que estou, levo unha cervexa para o camiño.", 
+    				"Que boa a...¿cuarta? cervexa. Cecáis debería comer algo, que onte empecéi así... Haberá por aquí algún sitio típico con tapas baratas de tortilla, albóndegas, calamares... Oh, oh, e chourizo con patacas, por suposto. E cervexa, a duda ofende",
     				1,
     				4,
     				"Cantos euros pagarías na bombilla por tres tapas?",
     				"3",
-    				"Bar La Bombilla, A coruña",
-    				"Eso sí, sigue pegando o sol que da gusto. Que ben me viña un xeado ahora mismo. Algo típico, caseiro, e que me quede preto, non penso andar un kilómetro para comerme un Cornetto.",
+    				"coruña tapas tipico barato",
+    				"ISTO XA É OUTRO CONTO. Onde vai parar. Creo que xa que estou, levo unha cervexa para o camiño.",     				
     				43370671,-8399557);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				" Que máis pode pedir un home? A barriga chea, moita cervexa, un bon xeado, unha mullero que o queir...",
+    				"Eso sí, sigue pegando o sol que da gusto. Que ben me viña un xeado ahora mismo. Algo típico, caseiro, e que me quede preto, non penso andar un kilómetro para comerme un Cornetto.",
     				1,
     				5,
     				"En que ano se abriu a Heladería Colón?",
     				"1973",
-    				"Heladería Colón, A Coruña",
-    				"O ANIVERSARIO .E onte nin cheguéi ó barco. Meu deus, desta mátame. Sólo me queda fuxir. Necesito un capitán cun barco que me poda levar lonxe de aquí.",
+    				"Heladería tipica coruña",
+    				" Que máis pode pedir un home? A barriga chea, moita cervexa, un bon xeado, unha muller que o queir...",    				
     				43369262,-8400703);
+    		
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Bueno, se aquí naide me pode levar a augas internacionáis, ninguén pode.", 
+    				"O ANIVERSARIO .E onte nin cheguéi ó barco. Meu deus, desta mátame. Sólo me queda fuxir. Necesito un capitán cun barco que me poda levar lonxe de aquí.",
     				1,
     				6,
     				"En que ano se fundou o Real Club Náutico Da Coruña?",
     				"1926",
-    				"Real Club Náutico de La Coruña",
-    				"Pois parece ser que onte escoitaron os berros da miña muller dende aquí e ahgora ninguén se atreve a levarme. Dinme que vaia a un centor comercial (din que hai varios para escoller, pero que non me complique) e que lle atope O REGALO PERFECTO para ela. Isto vai ser difícil.",
+    				"club nautico Coruña",
+    				"Bueno, se aquí naide me pode levar a augas internacionáis, ninguén pode.", 
     				43368454,-8397514);
+    		
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Atopeino! Se isto non consegue salvarme o pescozo, nada o fará. en fin, que sexa o que Deus queira. Tomarei outra cervexa, por se acaso.", 
+    				"Pois parece ser que onte escoitaron os berros da miña muller dende aquí e ahgora ninguén se atreve a levarme. Dinme que vaia a un centor comercial (din que hai varios para escoller, pero que non me complique) e que lle atope O REGALO PERFECTO para ela. Isto vai ser difícil.",
     				1,
     				7,
     				"Cantos centros comerciáis hai na Coruña?",
     				"6",
-    				"Centro de Ocio, El Puerto, A Coruña",
-    				"",
+    				"Centro comercial centro coruña",
+    				"Atopeino! Se isto non consegue salvarme o pescozo, nada o fará. En fin, que sexa o que Deus queira. Tomarei outra cervexa, por se acaso.", 
     				43368113,-840039);
+    		
+    		
+    		
+    		
     		
     		
     		
@@ -87,84 +96,82 @@ public class StartDataBase {
     				1,
     				"En que ano se construíu o Obelisco?",
     				"1895",
-    				"Obelisco Cantón Pequeno A Coruña",
-    				"Atopo un trozo de antracita que destaca sobor das demáis pedras. Atráeme estrañamente. Debaixo dela hai unha nota que di: Non todo está perdido. Sigue ás árbores, busca ó bardo de metal.",
+    				"Coruña monumento reloj linares",
+    				"Atopo un trozo de antracita que destaca sobor das demáis pedras. Atráeme estrañamente. Debaixo dela hai unha nota que di:",
     				43368659,-8402209);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Así que este é o famoso bardo. Gustaríame escoitar algunha das súas composicións. ", 
+    				"Non todo está perdido. Sigue ás árbores, busca ó bardo de metal.",
     				2,
     				2,
     				"En que ano foi asasinado John Lennon?",
     				"1980",
-    				"Monumendo a john Lennon, A Coruña",
-    				"Na súa man, hai outra nota: Nesta cidade viveu o Pintor dos períodos coloridos e das innumerábeis obras. Vai a onde él viveu.",
+    				"Estatua Musico Coruña",
+    				"Así que este é o famoso bardo. Gustaríame escoitar algunha das súas composicións.", 
     				43367712,-8403136);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Fun preguntando polo camiño, e foi un home realmente abraiante este Picasso, gustaríame dispoñer do tempo necesario para aprender máis, pero gustaríame máis volver á casa.", 
+    				"Na súa man, hai outra nota: Nesta cidade viveu o Pintor dos períodos coloridos e das innumerábeis obras. Vai a onde él viveu.",
     				2,
     				3,
     				"En cantos días pintou Picasso o Guernica?",
     				"33",
-    				"Casa Museo Picasso, A Coruña",
-    				"No seu buzón, unha nota: “Alí onde a terra divide ó mar e se alzan as bandeiras deberás continuar a túa viaxe.",
+    				"Casa Museo pintor, A Coruña",
+    				"Fun preguntando polo camiño, e foi un home realmente abraiante este Picasso, gustaríame dispoñer do tempo necesario para aprender máis, pero gustaríame máis volver á casa.", 
     				 43367156,-8407197);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Este sitio é especialmente belo. Creo que ficarei aquí un anaco a meditar sobre esta estraña situación e disfrutar da paisaxe.", 
+    				"No seu buzón, unha nota: “Alí onde a terra divide ó mar e se alzan as bandeiras deberás continuar a túa viaxe.",
     				2,
     				4,
     				"Cal é a festa máis coñecida celebrada nas praias de Orzán e Riazor?",
     				"San xoán",
-    				"Plaias de Riazor e Orzán,  A coruña.",
-    				"A última nota que atoipei, neste mesmo banco: Busca o lugar onde tódalas artes se xuntan, non moi lonxe da relixión e o mar.",
+    				"praia rompeolas coruña",
+    				"Este sitio é especialmente belo. Creo que ficarei aquí un anaco a meditar sobre esta estraña situación e disfrutar da paisaxe.", 
     				43369392,-8407937);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Resultoume especialmente complicado atopar con este sitio. Aínda así, marabilloume, a arte cambiou moito dende os meus días. ", 
+    				"A última nota que atoipei, neste mesmo banco: Busca o lugar onde tódalas artes se xuntan, non moi lonxe da relixión e o mar.",
     				2,
     				5,
-    				"En que ano se inaugurou o Museo Das Belas Artes Da Coruña?",
+    				"En que ano se inaugurou o Museo Das Belas Artes?" ,
     				"1995",
-    				"Museo Das Belas Artes Da Coruña",
-    				"Vai alí onde mora o Home.",
+    				"Museo artes coruña centro" ,
+    				"Resultoume especialmente complicado atopar con este sitio. Aínda así, marabilloume, a arte cambiou moito dende os meus días. ", 
     				43372806,-8399872);
-    		DatabaseOperations.getInstance(context).insertEvent(
-    				"Aquí non vive un home. Aquí vive O home.E fascíname este lugar. Tantas cousas que aprender...", 
+    		
+    		DatabaseOperations.getInstance(context).insertEvent( 
+    				"Vai alí onde mora o Home.",
     				2,
     				6,
     				"Cal é o lema escrito ás portas do museo?",
     				"Coñecete a ti mesmo",
-    				"Museo domus, A Coruña",
-    				"Continúa por este camiño. Tes outra morada que ir visitar.",
+    				"home casa coruña",
+    				"Aquí non vive un home. Aquí vive O home.E fascíname este lugar. Tantas cousas que aprender...",
     				43377511,-8406677);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Isto é realmente impresionante. E belo. Deberíase de coidar máis da natureza.", 
+    				"Continúa por este camiño. Tes outra morada que ir visitar.",
     				2,
     				7,
     				"Como se chama a sala máis famosa do Acuarium?",
     				"Nautilus",
-    				"Acuarium Finisterrae, A Coruña",
-    				"É posible que te atopes algo perdido. Xunto á cabeza de Xerión atoparás o Norte",
+    				"casa coruña cultura",
+    				"Isto é realmente impresionante. E belo. Deberíase de coidar máis da natureza.", 
     				43383507,-8409777);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"on todas as marabillas e cousas abraiantes que vin, resulta impresionante que unha Rosa dos Ventos non caera no olvido.", 
+    				"É posible que te atopes algo perdido. Xunto á cabeza de Xerión atoparás o Norte",
     				2,
     				8,
     				"Como se chamaba o petroleiro que naufragou non moi lonxe de aquí en 1992?",
-    				"Mar Egeo",
+    				"xerión norte coruña ",
     				"Rosa dos Ventos da Torre de Hércules, A Coruña",
-    				"Creo que xa sabes o seguinte paso. Achégate ó ceo todo o que podas, e descansa",
+    				"Con todas as marabillas e cousas abraiantes que vin, resulta impresionante que unha Rosa dos Ventos non caera no olvido.", 
     				43386698,-8407727);
     		DatabaseOperations.getInstance(context).insertEvent(
-    				"Xuntei todas as notas e parece que todo cobra sentido... Creo que a pesar de todo, voltarei á casa.", 
+    				"Creo que xa sabes o seguinte paso. Achégate ó ceo todo o que podas, e descansa",
     				2,
     				9,
     				"Cantos metros mide a Torre de Hércules?",
     				"68",
     				"Torre de Hércules",
-    				"",
+    				"Xuntei todas as notas e parece que todo cobra sentido... Creo que a pesar de todo, voltarei á casa.", 
     				4338569,-8406633);
         
         }
 }}
-
-//(String description, int historyId,  int order, String question, String answer, int search, String success,
-//int latitude, int longitude)
